223c6536e80d0928bddd2ac04752bf96548c9efe
Pistol <duc.nph.59cntt@ntu.edu.vn>
2020-04-29 17:33:42 +0700

    SimpleWidget

--- app/src/main/java/vn/edu/ntu/huuduc/huuduc_59130364_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/huuduc/huuduc_59130364_simplewidget/MainActivity.java
@@ -7,16 +7,21 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.security.cert.Extension;
+import java.text.MessageFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText edtten, edttuoi, edtST;
     Button btnXN;
     RadioGroup rdgchonGT;
+    RadioButton rdbNam, rdbNu;
     CheckBox cb1, cb2, cb3, cb4 ,cb5;
 
     @Override
@@ -31,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         edttuoi = findViewById(R.id.edttuoi);
         edtST = findViewById(R.id.edtST);
         rdgchonGT = findViewById(R.id.rdgchonGT);
+        rdbNam = findViewById(R.id.rdbNam);
+        rdbNu = findViewById(R.id.rdbNu);
         btnXN = findViewById(R.id.btnXN);
         cb1 = findViewById(R.id.cb1);
         cb2 = findViewById(R.id.cb2);
@@ -44,14 +51,44 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (v.getId() == R.id.btnXN) {
                     String ten = edtten.getText() + "";
-                    String tuoi = edttuoi.getText() + "";
+                    String tuoi = "Ngày Sinh: " + edttuoi.getText();
+                    String ST = edtST.getText() + "";
+                    String c1 = cb1.getText() + "";
+                    String c2 = cb2.getText() + "";
+                    String c3 = cb3.getText() + "";
+                    String c4 = cb4.getText() + "";
+                    String c5 = cb5.getText() + "";
+                    String nam = rdbNam.getText() + "";
+                    String nu = rdbNu.getText() + "";
                     ten = ten.trim();
                     tuoi = tuoi.trim();
-                    Toast.makeText(MainActivity.this, ten, Toast.LENGTH_LONG).show();
-                    Toast.makeText(MainActivity.this, tuoi, Toast.LENGTH_LONG).show();
+                    ST = ST.trim();
+                    String Vm = "Giới tính: ";
+                    if (rdbNam.isChecked()) {
+                        Vm += "Nam ";
+                    }
+                    if (rdbNu.isChecked()) {
+                        Vm += "Nữ ";
+                    }
+                    String msg = "Sở thích: ";
+                    if (cb1.isChecked()) {
+                        msg += "Xem Phim;";
+                    }
+                    if (cb2.isChecked()) {
+                        msg += " Nghe nhạc;";
+                    }
+                    if (cb3.isChecked()) {
+                        msg += " Đi cafe với bạn bè;";
+                    }
+                    if (cb4.isChecked()) {
+                        msg += " Ở nhà một mình;";
+                    }
+                    if (cb5.isChecked()) {
+                        msg += " Vào bếp nấu ăn;";
+                    }
+                    Toast.makeText(MainActivity.this, ten + "\n" + tuoi + "\n" + Vm +"\n" + msg + "\n" + ST, Toast.LENGTH_LONG).show();
                 }
             }
         });
-
     }
 }
